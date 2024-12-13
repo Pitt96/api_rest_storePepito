a9b153e8fc5b38f8a32689b82a81b4ff7f5290a3
pitt <roypalominor28@gmail.com>
2024-12-12 20:51:50 -0500

    Creación del proyecto ApiRest StorePepito

---
+++ src/main/java/com/ventas/apirest_ventas/ApirestVentasApplication.java
@@ -0,0 +1,13 @@
+package com.ventas.apirest_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestVentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ventas/apirest_ventas/ApirestVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventas.apirest_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
